7b0506b440208579284b9424a2cf4738519e8a76
Neelesh <saxena.ne@husky.neu.edu>
2016-12-08 00:06:24 -0500

    updating Git

--- GetMyFlight/app/models/Flight.scala
+++
@@ -1,9 +0,0 @@
-package models
-
-import play.api.libs.json._
-
-case class Flight(source: String, destination: String, dateOfTravel: String)
-object Flight {
-  
-  implicit val flightFormat = Json.format[Flight]
-}
